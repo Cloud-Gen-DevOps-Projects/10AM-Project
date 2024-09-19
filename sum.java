9b0bf40cfbbd9b2212bb58f466581b0b27c22f4c
Cloud Gen <ravindra@cloudgen.in>
2024-09-19 10:44:05 +0530

    V1.0

---
+++ sum.java
@@ -0,0 +1,14 @@
+class sum
+{
+	public static void main(Sting[] args)
+	{
+		int a,b,c;
+		a=70;
+		b=340;
+		c=123;
+		c=a+b+c;
+		System.out.println("\n The sum of three values are "+c);
+	}
+}
+
+
